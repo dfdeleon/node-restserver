7cb9b7f65980a0e792d0e7921c8d22b718f21e41
dfdeleon <floresdl.david@gmail.com>
2018-11-25 14:54:03 -0600

    Login normal seccion 10

--- server/config/config.js
+++ server/config/config.js
@@ -1,17 +1,33 @@
-// ====================
-//        Puerto
-// ====================
+// =========================
+//          Puerto
+// =========================
 process.env.PORT = process.env.PORT || 3000;
 
-// ====================
-//       Entorno
-// ====================
+// =========================
+//          Entorno
+// =========================
 
 process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 
-// ====================
-//     Base de Datos
-// ====================
+// =========================
+//   Vencimiento del token
+// =========================
+// 60 segundos
+// 60 minutos 
+// 24 horas
+// 30 días
+
+process.env.CADUCIDAD_TOKEN = 60 * 60 * 24 * 30;
+
+// =========================
+//  SEED de autenticación
+// =========================
+
+process.env.SEED = process.env.SEED || 'este-es-el-seed-desarrollo';
+
+// =========================
+//      Base de Datos
+// =========================
 let urlDB;
 
 if (process.env.NODE_ENV === 'dev') {
--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const Usuario = require('../models/usuario');
 const app = express();
 const bcrypt = require('bcrypt');
 const _ = require('underscore');
 
-app.get('/usuario', function(req, res) {
+const Usuario = require('../models/usuario');
+const { verificaToken, verificaAdmin_Role } = require('../middlewares/autenticacion')
+
+app.get('/usuario', verificaToken, (req, res) => {
 
     let desde = req.query.desde || 0;
     desde = Number(desde);
@@ -36,7 +38,7 @@ app.get('/usuario', function(req, res) {
         });
 });
 
-app.post('/usuario', function(req, res) {
+app.post('/usuario', [verificaToken, verificaAdmin_Role], (req, res) => {
 
     let body = req.body;
 
@@ -62,7 +64,7 @@ app.post('/usuario', function(req, res) {
     });
 });
 
-app.put('/usuario/:id', function(req, res) {
+app.put('/usuario/:id', [verificaToken, verificaAdmin_Role], (req, res) => {
     let id = req.params.id;
     let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
 
@@ -85,7 +87,7 @@ app.put('/usuario/:id', function(req, res) {
 
 });
 
-app.delete('/usuario/:id', function(req, res) {
+app.delete('/usuario/:id', [verificaToken, verificaAdmin_Role], (req, res) => {
 
     let id = req.params.id;
     // Usuario.findByIdAndRemove(id, (err, usuarioBorrado) => 
